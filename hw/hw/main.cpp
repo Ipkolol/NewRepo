a1610fac2f7984de990d90a5d13ee0a72029cd94
ipkolol <dubravec.filip@gmail.com>
2021-01-09 18:36:58 +0100

    commit do novy

--- hw/hw/main.cpp
+++ hw/hw/main.cpp
@@ -4,4 +4,5 @@ void main() {
 	std::cout << "novy git repo" << std::endl;
 	std::cout << "nejde to " << std::endl;
 	std::cout << "novy branch" << std::endl;
+	std::cout << "nove brench cosi" << std::endl;
 }
